a4ddb3cf1bb1fed7ad1e9789b2ff2d66cb87383f
Jeong Sang Un <sangun1993@gmail.com>
2019-09-05 09:15:24 +0900

    module -> class change

--- InfoManage.py
+++ InfoManage.py
@@ -1,38 +1,40 @@
-import infoModule
-import infoMoveModule
-import printModule
+from customModel import CustomInput
+from customControl import Move
+from customView import Printinfo
 
 def main():
-    move = infoMoveModule.Move()
+    control = Move()
+    view = Printinfo()
+    model = CustomInput()
     infoList = []
     infoCount = -1
     while True:
-        select = printModule.input_manual() #메뉴얼 출력 및 동작 입력
+        select = view.input_manual() #메뉴얼 출력 및 동작 입력
 
         if select == "I":    #고객 정보 입력
-            infoModule.inputInfo(infoList)
+            model.inputInfo(infoList)
 
         elif select == "C":  #현재 고객 정보 출력
-            move.nowInfo(infoCount,infoList)
-            printModule.printInfo(infoCount,infoList)
+            control.nowInfo(infoCount,infoList)
+            view.printInfo(infoCount,infoList)
 
         elif select == "N":  #다음 고객 정보 출력
-            infoCount = move.nextInfo(infoCount,infoList)
-            printModule.printInfo(infoCount,infoList)
+            infoCount = control.nextInfo(infoCount,infoList)
+            view.printInfo(infoCount,infoList)
             
         elif select == "P":  #이전 고객 정보 출력
-            infoCount = move.preInfo(infoCount,infoList)
-            printModule.printInfo(infoCount,infoList)
+            infoCount = control.preInfo(infoCount,infoList)
+            view.printInfo(infoCount,infoList)
     
         elif select == "A":  #모든 고객 정보 출력
-            printModule.printAllInfo(infoList)
+            view.printAllInfo(infoList)
 
         elif select == "U":  #현재 고객 정보 수정
-            infoCount = infoModule.updateInfo(infoCount,infoList)
-            printModule.printInfo(infoCount,infoList)
+            infoCount = model.updateInfo(infoCount,infoList)
+            view.printInfo(infoCount,infoList)
 
         elif select == "D":  #현재 고객 정보 삭제
-            infoCount = infoModule.deleteInfo(infoCount,infoList)
+            infoCount = model.deleteInfo(infoCount,infoList)
 
         elif select == "Q":  #프로그램 종료
             print("프로그램을 종료합니다")
